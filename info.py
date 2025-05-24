7664939fc23cbcaea2e254ed9c7082cb932e19c9
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2025-05-25 04:11:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,11 +29,11 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 BOT_START_TIME = time()
 
 # Bot images & videos
-PICS = (environ.get('PICS', 'https://ibb.co/Z1xNkYQm')).split()
-REQ_PICS = (environ.get('REQ_PICS', 'https://ibb.co/hFgTr8r1')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://ibb.co/mC8x7G99")
-MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/72dff2b65352ba85d0a34.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2a888a370f479f4338f7c.jpg")
+PICS = (environ.get('PICS', '')).split()
+REQ_PICS = (environ.get('REQ_PICS', '')).split()
+NOR_IMG = environ.get("NOR_IMG", "")
+MELCOW_VID = environ.get("MELCOW_VID", "")
+SPELL_IMG = environ.get("SPELL_IMG", "")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
